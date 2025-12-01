fd963acb09ef5eb41eb414524eaffdcf94d0d98a
Cristian-Aguilera-316 <158117506+Cristian-Aguilera-316@users.noreply.github.com>
2025-11-30 22:39:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Desafiolatam_M4D1/'
+  base: '/Desafiolatam_M4D1/',
+  build: {
+    outDir: 'docs'
+  }
 })
